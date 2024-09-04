b3c9bdb7fc2e3180fee27086bfe3a1f1373a444b
Stepan Suvorov <stevermeister@gmail.com>
2024-09-04 18:22:05 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// Constants
+const API_KEY = "sk-... YOUR OPENAI API KEY SHOULD BE HERE!!!";
+const ENDPOINT = "https://api.openai.com/v1/chat/completions";
+const MY_NAME = "Stepan Suvorov";
+const MODEL = "gpt-3.5-turbo";
+const MAX_TOKENS = 200;
